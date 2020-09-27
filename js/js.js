a81bb412206800f48a588a7ca5b1cac826ce51a2
David Martins <davidevandro@gmail.com>
2020-09-27 01:07:23 -0300

    Adicionando animacao para disparo do helicoptero

--- js/js.js
+++ js/js.js
@@ -18,6 +18,7 @@ function start() { // Inicio da função start()
 
     var velocidade=5;
     var posicaoY = parseInt(Math.random() * 334);
+    var podeAtirar=true;
 
     jogo.pressionou = [];
 
@@ -44,8 +45,7 @@ function start() { // Inicio da função start()
         movejogador();
         moveinimigo1();
         moveinimigo2();
-        moveamigo();
-        
+        moveamigo();        
 
     } // Fim da função loop()
 
@@ -82,7 +82,7 @@ function start() { // Inicio da função start()
         
         if (jogo.pressionou[TECLA.D]) {
             
-            //Chama função Disparo	
+            disparo();
         }
 
     } // fim da função movejogador()
@@ -124,5 +124,39 @@ function start() { // Inicio da função start()
             }
     
     } // fim da função moveamigo()
+
+    function disparo() {
+	
+        if (podeAtirar==true) {
+            
+            podeAtirar=false;
+            
+            topo = parseInt($("#jogador").css("top"))
+            posicaoX= parseInt($("#jogador").css("left"))
+            tiroX = posicaoX + 190;
+            topoTiro=topo+37;
+            $("#fundoGame").append("<div id='disparo'></div");
+            $("#disparo").css("top",topoTiro);
+            $("#disparo").css("left",tiroX);
+            
+            var tempoDisparo=window.setInterval(executaDisparo, 30);
+            
+        } //Fecha podeAtirar
+        
+        function executaDisparo() {
+            posicaoX = parseInt($("#disparo").css("left"));
+            $("#disparo").css("left",posicaoX+15); 
+        
+            if (posicaoX>900) {
+                                
+                window.clearInterval(tempoDisparo);
+                tempoDisparo=null;
+                $("#disparo").remove();
+                podeAtirar=true;
+                            
+            }
+        } // Fecha executaDisparo()
+    } // Fecha disparo()
+    
 
 } // Fim da função start
